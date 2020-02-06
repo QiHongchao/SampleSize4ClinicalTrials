146d1decf4edbd2e2fc99549892fb58dcfb2f087
Hongchao Qi <32048822+QiHongchao@users.noreply.github.com>
2020-02-06 11:36:58 +0100

    update ssc_meancomp function

--- R/ssc_meancomp.R
+++ R/ssc_meancomp.R
@@ -15,19 +15,19 @@
 #' @param ratio The ratio between the number of subjects in the treatment arm and that in the control arm
 #' @param alpha Type I error rate
 #' @param power Statistical power of the test (1-type II error rate)
-#' @param sigma The variance of observed outcomes in both arms
+#' @param var The variance of observed outcomes in both arms
 #' @param theta The true mean difference between two arms
 #' @param delta The prespecified superiority, non-inferiority or equivalence margin
 #'
 #' @return samplesize
 #'
-#' @usage ssc_meancomp(design, ratio, alpha, power, sigma, theta, delta)
+#' @usage ssc_meancomp(design, ratio, alpha, power, var, theta, delta)
 #'
 #' @importFrom stats qnorm
 #'
 #' @examples
 #' ##The comparison of means, a non-inferiority trial and the non-inferiority margin is -0.05
-#' ssc_meancomp(design=3L, ratio=1, alpha=0.05, power=0.8, sigma=0.01, theta=0, delta=-0.05)
+#' ssc_meancomp(design=3L, ratio=1, alpha=0.05, power=0.8, var=0.01, theta=0, delta=-0.05)
 #'
 #' @references
 #' Chow S, Shao J, Wang H. 2008. Sample Size Calculations in Clinical Research. 2nd Ed. Chapman & Hall/CRC Biostatistics Series.
@@ -37,21 +37,21 @@
 #' @export
 
 ##Sample size calculation for mean comparison
-ssc_meancomp<-function(design = c(1L,2L,3L,4L), ratio = 1, alpha = 0.05, power = 0.8, sigma = NULL,theta = NULL, delta = NULL){
+ssc_meancomp<-function(design = c(1L,2L,3L,4L), ratio = 1, alpha = 0.05, power = 0.8, var = NULL,theta = NULL, delta = NULL){
   if (!(design %in% 1L:4L))
     stop("Unrecognized study design, 1: Test for equility, 2: Superiority trial,
          3: Non-inferiority trial, 4: Equivalence trial")
   ##Numerator
   if (design==1L) {
-    numerator <- (abs(qnorm(alpha/2)) + abs(qnorm(1- power)))^2 * sigma * (1 + 1/ratio)
+    numerator <- (abs(qnorm(alpha/2)) + abs(qnorm(1- power)))^2 * var * (1 + 1/ratio)
   }
   ##Note that we unify the superiority and non-inferiority trials in this version (refer to Chow et al.)
   ##In the last version, there was no superiority margin and the non-inferiority margin was positive (refer to Yin).
   if (design %in% 2L:3L) {
-    numerator <- (abs(qnorm(alpha)) + abs(qnorm(1 - power)))^2 * sigma * (1 + 1/ratio)
+    numerator <- (abs(qnorm(alpha)) + abs(qnorm(1 - power)))^2 * var * (1 + 1/ratio)
   }
   if (design == 4L) {
-    numerator <- (abs(qnorm(alpha)) + abs(qnorm((1 - power)/2)))^2 * sigma * (1 + 1/ratio)
+    numerator <- (abs(qnorm(alpha)) + abs(qnorm((1 - power)/2)))^2 * var * (1 + 1/ratio)
   }
 
   ##Denominator
